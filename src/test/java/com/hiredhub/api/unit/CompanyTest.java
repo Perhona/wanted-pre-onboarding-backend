367715c6501fc2eb0d8c0562eacccaaf1c07145a
perhona <perhona@kakao.com>
2024-08-10 16:46:16 +0900

    test: 회사의 다른 공고 조회 단위 테스트 추가

--- src/test/java/com/hiredhub/api/unit/CompanyTest.java
+++ src/test/java/com/hiredhub/api/unit/CompanyTest.java
@@ -5,6 +5,8 @@ import com.hiredhub.api.model.JobPosting;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CompanyTest {
@@ -16,5 +18,18 @@ public class CompanyTest {
 
         company.addJobPosting(jobPosting);
         assertThat(company.getJobPostings()).contains(jobPosting);
+    }
+
+    @DisplayName("회사의 다른 채용 공고 id 조회")
+    @Test
+    void getOtherJobPostingIds() {
+        Company company = new Company(1L, "hiredhub");
+        JobPosting jobPosting = new JobPosting(1L, "백엔드 개발자", "한국", "서울", 10_000_000, "java", "백엔드 개발자를 모집합니다.", company);
+        JobPosting jobPosting2 = new JobPosting(2L, "백엔드 개발자", "한국", "서울", 10_000_000, "java", "백엔드 개발자를 모집합니다.", company);
+        company.addJobPosting(jobPosting);
+        company.addJobPosting(jobPosting2);
+
+        List<Long> otherJobPostingIds = company.getOtherJobPostingIds(jobPosting.getId());
+        assertThat(otherJobPostingIds).hasSize(1);
     }
 }
